25253d4758c8f00b0afeec01707b2925b47d3340
djzh23 <zouh.ijd@gmail.com>
2025-10-30 15:03:08 +0100

    feat: Add initial ownerService and migrations

---
+++ src/MiniVehicleAPI.Application/Vehicles/OwnerService.cs
@@ -0,0 +1,29 @@
+﻿using MiniVehicleAPI.Domain.Abstractions;
+using MiniVehicleAPI.Domain.Entities;
+
+namespace MiniVehicleAPI.Application.Vehicles;
+
+public class OwnerService
+{
+    // repository injection
+    private readonly IRepository<Owner> _repo;
+
+    public OwnerService(IRepository<Owner> repository) => _repo = repository;
+
+    // GetByIdAsync From IRepository interface ( _repo ) 
+    // Task<T?> GetByIdAsync(int id, CancellationToken ct = default);
+    public async Task<OwnerReadDto?> GetAsync(int id, CancellationToken ct = default)
+    {
+        //OwnerReadDto owner = await _repo.GetByIdAsync(id, ct);
+        //return owner;
+        return null;
+    }
+
+    // Task AddAsync(T entity, CancellationToken ct = default);
+    public async Task<int> CreateOwnerAsync(Owner owner, CancellationToken ct = default)
+    {
+        await _repo.AddAsync(owner, ct);
+        await _repo.SaveChangesAsync(ct);
+        return owner.Id;
+    }
+}
---
+++ src/miniVehicleAPI.Infrastructure/Migrations/20251029132842_AddOwnerAndRelationship.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MinivehicleAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerAndRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdateAtUtc",
+                table: "Vehicles",
+                newName: "UpdatedAtUtc");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateAtUtc",
+                table: "Vehicles",
+                newName: "CreatedAtUtc");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerId",
+                table: "Vehicles",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Owner",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Firstname = table.Column<string>(type: "text", nullable: false),
+                    Lastname = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owner", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_OwnerId",
+                table: "Vehicles",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Owner_OwnerId",
+                table: "Vehicles",
+                column: "OwnerId",
+                principalTable: "Owner",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Owner_OwnerId",
+                table: "Vehicles");
+
+            migrationBuilder.DropTable(
+                name: "Owner");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vehicles_OwnerId",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Vehicles");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAtUtc",
+                table: "Vehicles",
+                newName: "UpdateAtUtc");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedAtUtc",
+                table: "Vehicles",
+                newName: "CreateAtUtc");
+        }
+    }
+}
