1464934178bea391e4570263fc2426241016cc65
djzh23 <zouh.ijd@gmail.com>
2025-10-30 22:46:59 +0100

    feat: Add OwnerDtos and first try service and controller

---
+++ src/MiniVehicleAPI.Application/Owners/OwnerDtos.cs
@@ -0,0 +1,7 @@
+﻿namespace MiniVehicleAPI.Application.Owners;
+
+
+public record OwnerReadDto(int Id, string Firstname, string Lastname);
+public record OwnerCreateDto(string Firstname, string Lastname, string Email, string? Phone);
+public record OwnerUpdateDto(string? Firstname, string? Lastname, string? Email, string? Phone);
+
---
+++ src/MiniVehicleAPI.Application/Owners/OwnerService.cs
@@ -0,0 +1,47 @@
+﻿using MiniVehicleAPI.Domain.Abstractions;
+using MiniVehicleAPI.Domain.Entities;
+
+namespace MiniVehicleAPI.Application.Owners;
+
+public class OwnerService
+{
+    // repository injection
+    private readonly IRepository<Owner> _repo;
+
+    public OwnerService(IRepository<Owner> repository) => _repo = repository;
+
+    // GetByIdAsync From IRepository interface ( _repo ) 
+    // Task<T?> GetByIdAsync(int id, CancellationToken ct = default);
+    public async Task<OwnerReadDto?> GetAsync(int id, CancellationToken ct = default)
+    {
+        //OwnerReadDto owner = await _repo.GetByIdAsync(id, ct);
+        // get owner from repository
+        Owner? ownerEntity = await _repo.GetByIdAsync(id, ct);
+
+        // check if the owner is null 
+        // return null and logging ...
+        if (ownerEntity == null)
+        {
+            // Optional: _logger.LogWarning("Owner with ID {Id} not found.", id);
+            return null;
+        }
+
+        // map Owner to OwnerReadDto
+        var ownerDto = new OwnerReadDto(
+            Id: ownerEntity.Id,
+            Firstname: ownerEntity.Firstname,
+            Lastname: ownerEntity.Lastname
+        );
+
+        // return OwnerReadDto
+        return ownerDto;
+    }
+
+    // Task AddAsync(T entity, CancellationToken ct = default);
+    public async Task<int> CreateOwnerAsync(Owner owner, CancellationToken ct = default)
+    {
+        await _repo.AddAsync(owner, ct);
+        await _repo.SaveChangesAsync(ct);
+        return owner.Id;
+    }
+}
